f4aebb2d4d1e98898e9884163ebb65c4c6c3c940
Daniel Lemky <daniel@quantumfirelabs.com>
2022-01-19 21:22:31 -0600

    Added missing associations

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,10 @@
 class Client < ApplicationRecord
   has_many :client_proposals
+  has_one :alias
+  has_one :personal_detail
+  has_many :phone_numbers
+  has_one :current_address
+  has_one :identification
+  has_many :employments
   enum role:{ primary: "primary", secondary: "secondary" }
 end
