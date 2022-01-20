75a3e7ae0fe4448804a57a243070163f7c26eff6
Daniel Lemky <daniel@quantumfirelabs.com>
2022-01-19 20:59:34 -0600

    User model

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  enum role:{ administrator: "administrator", supervisor: "supervisor", agent: "agent" }
+
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :agent
+  end
+end
